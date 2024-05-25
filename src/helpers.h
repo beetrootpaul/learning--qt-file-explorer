3f9b05475580bdbf42c20c55fc759369c953928f
Beetroot Paul <paul@beetrootpaul.com>
2024-05-25 16:03:23 +0200

    extract helpers

---
+++ src/helpers.h
@@ -0,0 +1,8 @@
+#ifndef LEARNING__QT_FILE_EXPLORER_HELPERS_H
+#define LEARNING__QT_FILE_EXPLORER_HELPERS_H
+
+void examine(const std::string &label, const QStringList &paths);
+
+void examine(const std::string &label, const QString &path);
+
+#endif //LEARNING__QT_FILE_EXPLORER_HELPERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -11,76 +11,24 @@
 #include <QStandardPaths>
 #include <QToolBar>
 
+#include "helpers.h"
 #include "directory_picker/DirectoryPickerWidget.h"
-
-// TODO: extract to a separate file
-void examine(const std::string &label, const QString &path) {
-
-    auto normalizedPath = QDir::toNativeSeparators(path);
-
-    std::cout << std::endl;
-
-    std::cout << "### " << label << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "path: " << normalizedPath.toStdString() << std::endl;
-
-    std::cout << std::endl;
-
-    QDir dir(normalizedPath);
-    std::cout << "does dir exist? " << (dir.exists() ? "yes" : "!!! NO !!!") << std::endl;
-
-    std::cout << std::endl;
-
-    // NOTE: This one does print only if e.g. IDE got permissions to access those folders,
-    //       granted by the user.
-    std::cout << "entries: " << std::endl;
-    dir.setFilter(QDir::Dirs | QDir::Files | QDir::NoDot);
-    for (const auto &fileInfo: dir.entryInfoList()) {
-        std::cout << "- " << fileInfo.fileName().toStdString();
-        if (fileInfo.isDir()) {
-            std::cout << QDir::separator().toLatin1();
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << std::endl;
-}
-
-// TODO: extract to a separate file
-void examine(const std::string &label, QStringList paths) {
-    unsigned i = 1;
-    for (const auto &path: paths) {
-        std::stringstream ss;
-        ss << "(" << i << "/" << paths.count() << ") " << label;
-        examine(ss.str(), path);
-        i += 1;
-    }
-}
-
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     examine("HOME",
             QDir::homePath());
-
     examine("minecraft server",
             QDir::homePath().append("/minecraft_server_forge_1_20_1"));
-
     examine("invalid path",
             QDir::homePath().append("/does-not-exist"));
-
     examine("Developer",
             QDir::homePath().append("/Developer"));
-
     examine("downloads",
             QStandardPaths::standardLocations(QStandardPaths::DownloadLocation));
-
     examine("documents",
             QStandardPaths::standardLocations(QStandardPaths::DocumentsLocation));
-
     examine("app local data",
             QStandardPaths::standardLocations(QStandardPaths::AppLocalDataLocation));
 
@@ -95,7 +43,7 @@ int main(int argc, char *argv[]) {
     mainWindow.resize(800, 600);
 
     auto directoryPicker = new directory_picker::DirectoryPickerWidget();
-    
+
     mainWindow.setCentralWidget(directoryPicker->getWidget());
 
     // TODO: and what?
