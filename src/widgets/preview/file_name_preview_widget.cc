61d62419c5303f781f50e704d2720df08616a88a
Beetroot Paul <paul@beetrootpaul.com>
2024-05-30 00:13:13 +0200

    nicer file name rendering

--- src/widgets/preview/file_name_preview_widget.cc
+++ src/widgets/preview/file_name_preview_widget.cc
@@ -1,6 +1,7 @@
 #include "file_name_preview_widget.h"
 
 #include <QFileInfo>
+#include <QSizePolicy>
 
 namespace qt_file_explorer::widgets {
 
@@ -13,7 +14,18 @@ FileNamePreviewWidget::~FileNamePreviewWidget() {
 }
 
 // TODO: enforce it through interface
-void FileNamePreviewWidget::init() {}
+void FileNamePreviewWidget::init() {
+  // Prevent this QLabel from expanding to match longer file names
+  setSizePolicy(
+      QSizePolicy(QSizePolicy::Policy::Ignored, QSizePolicy::Policy::Ignored));
+
+  // TODO: implement MD rendered and use this:
+  //  setTextFormat(Qt::TextFormat::MarkdownText);
+
+  setMargin(8);
+  setAlignment(Qt::AlignTop);
+  setWordWrap(true);
+}
 
 bool FileNamePreviewWidget::canPreview(const QString& path) {
   return true;
@@ -22,6 +34,7 @@ bool FileNamePreviewWidget::canPreview(const QString& path) {
 void FileNamePreviewWidget::preview(QString path) {
   QFileInfo fileInfo(path);
   setText(fileInfo.fileName());
+  // TODO: make it e.g. centered
 }
 
 void FileNamePreviewWidget::clear() {
